27e21ff9871045f6af4aedb9c525b74e7830bab3
Artur Höppner <artur.hoppner@umain.com>
2025-04-01 12:16:44 +0200

    added stega to client

--- src/sanity/lib/client.ts
+++ src/sanity/lib/client.ts
@@ -1,10 +1,12 @@
 import { createClient } from "next-sanity"
 
 import { apiVersion, dataset, projectId } from "../env"
+const baseUrl = process.env.NEXT_PUBLIC_BASE_URL ?? "http://localhost:3002"
 
 export const client = createClient({
   projectId,
   dataset,
   apiVersion,
-  useCdn: true // Set to false if statically generating pages, using ISR or tag-based revalidation
+  useCdn: true, // Set to false if statically generating pages, using ISR or tag-based revalidation
+  stega: { studioUrl: baseUrl, enabled: true }
 })
