6df3ec3e22b23401a91526dda4faa12be726fdb2
Artur Höppner <artur.hoppner@umain.com>
2025-04-01 12:08:51 +0200

    Updated next config to remove console on production and let sanity handled cdn

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,22 @@
 import type { NextConfig } from "next"
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  compiler: {
+    removeConsole: process.env.NODE_ENV === "production"
+  },
+  images: {
+    unoptimized: true, // Let sanity CDN handle optimization of images.
+    dangerouslyAllowSVG: true,
+    contentDispositionType: "attachment",
+    contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "cdn.sanity.io",
+        port: ""
+      }
+    ]
+  }
 }
 
 export default nextConfig
