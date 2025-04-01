c8c70fe5d58467a71e60bad886909bf42648c1fd
Artur Höppner <artur.hoppner@umain.com>
2025-04-01 10:54:54 +0200

    Structure and some sanity config update

--- sanity.config.ts
+++ sanity.config.ts
@@ -5,24 +5,29 @@
  */
 
 import {visionTool} from '@sanity/vision'
-import {defineConfig} from 'sanity'
+import {defineConfig, isDev} from 'sanity'
 import {structureTool} from 'sanity/structure'
 
 // Go to https://www.sanity.io/docs/api-versioning to learn how API versioning works
 import {apiVersion, dataset, projectId} from './src/sanity/env'
 import {schema} from './src/sanity/schemaTypes'
 import {structure} from './src/sanity/structure'
+import {HomeIcon } from '@sanity/icons'
 
 export default defineConfig({
   basePath: '/studio',
   projectId,
   dataset,
+  title: isDev ? `dataset ${dataset}` : "Template Project",
+  icon: HomeIcon,
   // Add and edit the content schema in the './sanity/schemaTypes' folder
   schema,
-  plugins: [
+  plugins: isDev ? [
     structureTool({structure}),
+    visionTool({defaultApiVersion: apiVersion}),
     // Vision is for querying with GROQ from inside the Studio
     // https://www.sanity.io/docs/the-vision-plugin
-    visionTool({defaultApiVersion: apiVersion}),
+  ] : [
+    structureTool({structure}),
   ],
 })
